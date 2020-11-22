bd28d8c41d9c8f20f034b0386cfe939396bc9847
Sylwester Miecznikowski <sylwester.miecznikowski@gmail.com>
2020-11-22 14:45:48 +0100

    working calculator

--- scripts/displayData.js
+++ scripts/displayData.js
@@ -1,15 +1,112 @@
-let pressedValue = 0;
+let operatorsArray = ["+", "-", "*", "/"];
+let currentInput = '';
+let newValue = '';
+let globalOperator = '';
+let previousValue = '';
+let secondValue = '';
+let firstValue = 0;
+let result = '';
 
-const pressedButton = document.querySelectorAll("div.calculator-buttons button");
-const displayResult = document.querySelector("div.main-display");
+function updateValues(e) {
+    switch (newValue) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "0":
+            if(currentInput.length > 14){
+                throw 'Max 14 digits';
+            }
+            previousValue = newValue;
+            currentInput += newValue;
+            updateDisplay(currentInput);
+            break;
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+            if(operatorsArray.includes(previousValue)){
+                globalOperator = newValue;
+            } else if(!operatorsArray.includes(previousValue) && globalOperator === '') {
+                manageOperator(newValue);
+            } else if(firstValue !== '' && currentInput !== "" && globalOperator !== ""){
+                secondValue = currentInput;
+                result = operate(globalOperator, firstValue, secondValue);
+                if(operatorsArray.includes(newValue)){
+                    globalOperator = newValue;
+                }
+                firstValue = result;
+                updateDisplay(result);
+                currentInput = '';
+                secondValue = '';
+            } else if (!operatorsArray.includes(previousValue) && globalOperator !== ''){
+                manageOperator(newValue);
+            }
+            break;
+        case "=":
+            if(firstValue !== '' && currentInput !== ''){
+                if(currentInput !== "" && globalOperator !== ""){
+                    secondValue = currentInput;
+                    result = operate(globalOperator, firstValue, secondValue);
+                    updateDisplay(result);
+                    currentInput = result;
+                    newValue = '';
+                    globalOperator = '';
+                    previousValue = '';
+                    secondValue = '';
+                    firstValue = result;
+                }
+            }
+            break;
+        case "clear":
+            currentInput = '';
+            newValue = '';
+            globalOperator = '';
+            previousValue = '';
+            secondValue = '';
+            firstValue = 0;
+            result = '';
+            updateDisplay('0');
 
-function displayData() {
+    }
+}
+
+function manageOperator(operator) {
+    previousValue = operator;
+    globalOperator = operator;
+    if(result !== ''){
+        firstValue = result;
+    } else {
+        firstValue = currentInput;
+    }
+    currentInput = '';
+    console.log(globalOperator);
+}
+
+function updateDisplay(e){
+    const displayData = document.querySelector("div.main-display");
+    displayData.textContent = `${e}`;
+}
+
+function buttonClick() {
+    const pressedButton = document.querySelectorAll("div.calculator-buttons button");
     pressedButton.forEach((button) => {
-        button.addEventListener('click', () => {
-            pressedValue = button.value;
-            displayResult.innerHTML = pressedValue;
+        button.addEventListener('click', (e) => {
+            try{
+                newValue = button.value;
+                updateValues(e);
+            } catch (err) {
+                console.log(err);
+            }
+
         });
     });
 }
 
-displayData();
+updateDisplay('0');
+buttonClick();
--- scripts/operations.js
+++ scripts/operations.js
@@ -1,28 +1,30 @@
 function operate(operator, firstNumber, secondNumber) {
     switch(operator) {
-        case 'add' :
+        case '+' :
             return add(firstNumber, secondNumber);
-        case 'subtract' :
+        case '-' :
             return subtract(firstNumber, secondNumber);
-        case 'multiply' :
-            return multiply(firstNumber, secondNumber);
-        case 'divide' :
-            return divide(firstNumber, secondNumber);
+        case '*' :
+            return Math.round(multiply(firstNumber, secondNumber).toFixed(2) * 100) / 100;
+        case '/' :
+            if(firstNumber === '0' || secondNumber === '0'){
+                return "ERROR";
+            }
+            return Math.round(divide(firstNumber, secondNumber).toFixed(2) * 100) / 100;
         default:
             console.log('ERROR');
     }
 }
 
 function add(a, b) {
-    return a+b;
+    return parseFloat(a)+parseFloat(b);
 }
 function subtract(a, b) {
-    return a-b;
+    return parseFloat(a)-parseFloat(b);
 }
 function multiply(a, b) {
-    return a*b;
+    return parseFloat(a)*parseFloat(b);
 }
 function divide(a, b) {
-    return a/b;
+    return parseFloat(a)/parseFloat(b);
 }
-
